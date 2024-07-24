af8fa9c1d603992987684a1ee581f9e31949abce
call-by <smart.solidity@gmail.com>
2024-07-24 05:52:38 -0400

    feat: LMFactory and LM contract initialization and deployment using CREATE2

---
+++ contracts/LiquidityManagerFactory.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {LiquidityManager} from "./LiquidityManager.sol";
+import {ILiquidityManagerFactory} from "./interfaces/ILiquidityManagerFactory.sol";
+
+contract LiquidityManagerFactory is Ownable, ILiquidityManagerFactory {
+    error LiquidityManagerAlreadyExists();
+
+    struct Parameters {
+        address factory;
+        address nfpm;
+        address pool;
+        uint8 poolType;
+    }
+
+    /// @inheritdoc ILiquidityManagerFactory
+    Parameters public override parameters;
+    // poolType 0: Stable pool params: +/- 1% range: 5bps
+    // poolType 1: Blue chip pool params: +/- 10% range: 30bps
+    // poolType 2: Small cap pool params: +/- 50% range: 100bps
+    // pool => liquidity manager
+    mapping(address => address) public liquidityManagers;
+
+    address public immutable nfpm; // Univ3 NonFungiblePositionManager
+
+    constructor(address _nfpm) {
+        nfpm = _nfpm;
+    }
+
+    function deployLiquidityManager(address pool, uint8 poolType) external onlyOwner {
+        if (liquidityManagers[pool] != address(0)) {
+            revert LiquidityManagerAlreadyExists();
+        }
+
+        parameters = Parameters({factory: address(this), nfpm: nfpm, pool: pool, poolType: poolType});
+        address liquidityManager = address(new LiquidityManager{salt: keccak256(abi.encode(pool, poolType))}());
+        liquidityManagers[pool] = liquidityManager;
+
+        delete parameters;
+    }
+}
