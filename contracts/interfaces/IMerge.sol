b4c2d38b62b845c08fe6e1afcfc7e12ced220833
jp-smart <joshupgig@gmail.com>
2024-08-02 14:17:04 -0400

    fix: update merge contract status

---
+++ contracts/interfaces/IMerge.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+interface IMerge {
+    enum LockedStatus {
+        Locked,
+        OneWay,
+        TwoWay
+    }
+
+    error MergeLocked();
+    error VultToWeweNotAllwed();
+    error InvalidToken();
+}
