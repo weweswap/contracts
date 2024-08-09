c14cf18dbe040fd8c01cb0a4eda457f726bcd0cb
jp-smart <joshupgig@gmail.com>
2024-08-09 12:13:37 -0400

    feat: remove unused interfaces

--- contracts/interfaces/IERC20Vult.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-interface IERC20Vult is IERC20 {
-    function approveAndCall(address spender, uint256 amount, bytes calldata extraData) external returns (bool);
-}
--- contracts/interfaces/IWhitelist.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IWhitelist {
-    function checkWhitelist(address from, address to, uint256 amount) external;
-}
