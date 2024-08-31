c52e4f5c1e49baf8ffb8ddce9379d3fe477089b9
hippocampusWeb3 <hippocampus.web3@proton.me>
2024-08-31 17:49:03 +0200

    Check position pair onReceive in migration contract

--- scripts/stoleNftPositionFromAddress.ts
+++ scripts/stoleNftPositionFromAddress.ts
@@ -8,10 +8,10 @@ const ercAbiErc721 = [
 async function main() {
     let signers = await ethers.getSigners();
 
-    const ownerAddress = "0x38019bC40f504BE4546F24083Ccaf0c8553C408A";
+    const ownerAddress = "0x32cf4d1df6fb7bB173183CF8b51EF9499c803634";
     const nonfungiblePositionManager = "0x03a520b32C04BF3bEEf7BEb72E919cf822Ed34f1";
     const recipientAddress = signers[0].address;
-    const tokenId = 888441; 
+    const tokenId = [120852, 123540, 176785, 176791, 196270, 240357, 258321, 278844]; 
 
     await network.provider.request({
         method: "hardhat_impersonateAccount",
@@ -22,10 +22,11 @@ async function main() {
 
     const nftContract = await ethers.getContractAt(ercAbiErc721, nonfungiblePositionManager, impersonatedSigner);
 
-    const tx = await nftContract.transferFrom(ownerAddress, recipientAddress, tokenId);
-    await tx.wait();
-
-    console.log(`Transfer complete: NFT with ID ${tokenId} from ${ownerAddress} to ${recipientAddress}`);
+    for (let index = 0; index < tokenId.length; index++) {
+        const tx = await nftContract.transferFrom(ownerAddress, recipientAddress, tokenId[index]);
+        await tx.wait();
+        console.log(`Transfer complete: NFT with ID ${tokenId[index]} from ${ownerAddress} to ${recipientAddress}`);
+    }
 
     await network.provider.request({
         method: "hardhat_stopImpersonatingAccount",
