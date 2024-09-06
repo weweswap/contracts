c46ca5a479d775117f02e2542d4ac8f4d008d983
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-06 17:26:43 +1000

    wip

--- contracts/token/ChaosToken.sol
+++ contracts/token/ChaosToken.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ChaosToken is IERC20 {
-    constructor() ERC20("ChaosToken", "CHAOS") {
-        _mint(msg.sender, 100_000_000 * 10 ** decimals());
+contract ChaosToken is ERC20, Ownable {
+    constructor() ERC20("$CHAOS", "CHAOS") {}
+
+    function mint(uint256 amount) public onlyOwner {
+        super._mint(address(this), amount);
     }
 }
