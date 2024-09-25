5c704f2b59320571be6154148c7d128ea4ffe87a
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-09-25 15:58:11 +1000

    Update deployed addresses and artifacts for FarmModule#Farm

--- ignition/modules/Farm.ts
+++ ignition/modules/Farm.ts
@@ -3,8 +3,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 // import addresses from "../deployments/chain-8453/deployed_addresses.json"
 
 export default buildModule("FarmModule", m => {
-	// const CHAOS_ADDRESS = addresses["ChaosTokenModule#ChaosToken"] || "0xCb69EAaFE84D639021192398c1d2DB0d97AA13aA";
-	const CHAOS_ADDRESS = "0xCb69EAaFE84D639021192398c1d2DB0d97AA13aA";
+	const CHAOS_ADDRESS = "0xf901F4ec62590231f374cA7C5d34BbcA49B11d6B";
 	const farm = m.contract("Farm", [CHAOS_ADDRESS]);
 
 	return { farm };
