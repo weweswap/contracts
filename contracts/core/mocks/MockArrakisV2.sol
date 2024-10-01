be336482ca1ced2ef5dc82ab5630a63b7a28a39c
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-01 10:09:48 +1000

    Refactor MockArrakisV2.sol and add MockToken.sol

--- contracts/core/mocks/MockArrakisV2.sol
+++ contracts/core/mocks/MockArrakisV2.sol
@@ -2,12 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MockToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 1000000 * 10 ** 18);
-    }
-}
+import "./MockToken.sol";
 
 contract MockArrakisV2 {
     IERC20 public token;
