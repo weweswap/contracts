c8a02d5541e03c150a412e16f75b625a0c7a2506
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-03 11:30:34 +1000

    Refactor Eater contract to handle token merging and pausing
    - Add Pausable contract from OpenZeppelin to Eater contract
    - Modify _merge function to use whenNotPaused modifier
    - Add togglePause function to allow owner to pause/unpause contract
    - Add sweep function to allow owner to sweep remaining Wewe tokens
    
    Co-authored-by: [Author Name]

--- contracts/core/merge/Eater.sol
+++ contracts/core/merge/Eater.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.19;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "../../interfaces/IWeweReceiver.sol";
 
-abstract contract Eater is IWeweReceiver, ReentrancyGuard, Ownable {
+abstract contract Eater is IWeweReceiver, ReentrancyGuard, Pausable, Ownable {
     uint256 internal _rate;
     address internal _token;
     address public wewe;
@@ -21,7 +22,7 @@ abstract contract Eater is IWeweReceiver, ReentrancyGuard, Ownable {
         }
     }
 
-    function _merge(uint256 amount, address token, address from) internal {
+    function _merge(uint256 amount, address token, address from) internal whenNotPaused {
         uint256 weweToTransfer = (amount * _rate) / 100;
         require(
             weweToTransfer <= IERC20(wewe).balanceOf(address(this)),
@@ -34,8 +35,14 @@ abstract contract Eater is IWeweReceiver, ReentrancyGuard, Ownable {
         emit Merged(amount, from);
     }
 
+    function sweep() external onlyOwner {
+        uint256 balance = IERC20(wewe).balanceOf(address(this));
+        require(balance > 0, "Eater: No balance to sweep");
+        IERC20(wewe).transfer(wewe, balance);
+    }
+
     /// @notice Wewe token approveAndCall function
-    function receiveApproval(address from, uint256 amount, address token, bytes calldata) external nonReentrant {
+    function receiveApproval(address from, uint256 amount, address token, bytes calldata) external nonReentrant whenNotPaused {
         // After wewe approve and call, it will call this function
         require(_token != address(0), "GenericEater: Token address not set");
         require(msg.sender == wewe, "GenericEater: Invalid sender");
@@ -43,6 +50,14 @@ abstract contract Eater is IWeweReceiver, ReentrancyGuard, Ownable {
 
         // Eat the underlying token "_token" with the amount of "amount"
         _merge(amount, _token, from);
+    }
+
+    function togglePause() external onlyOwner {
+        if (paused()) {
+            _unpause();
+        } else {
+            _pause();
+        }
     }
 
     function _deposit(uint256 amount) internal {
