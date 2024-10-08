6547fba367df7408e854be944e1db1e5d4558cf3
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-08 21:16:35 +1000

    feat: create a dynamic fee contract

--- contracts/core/amm/UniswapDynamic.sol
+++ contracts/core/amm/UniswapDynamic.sol
@@ -7,31 +7,21 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 
-contract Uniswapv3 is IAMM, Ownable {
+contract UniswapDynamicFee is IAMM, Ownable {
     address private router = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     address private wewe = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
-    uint24 public fee;
-
-    constructor() Ownable() {
-        fee = 3000;
-    }
-
-    function setFee(uint24 _fee) external onlyOwner {
-        if (_fee != 500 && _fee != 3000 && _fee != 10000) {
-            revert("Uniswapv3: Invalid fee");
-        }
-
-        fee = _fee;
-    }
+    constructor() Ownable() {}
 
     function buy(uint256 amount, address token, bytes calldata extraData) external returns (uint256) {
-        uint256 amountOut = _swap(wewe, token, amount, 0);
+        uint24 fee = uint24(uint256(keccak256(extraData)));
+        uint256 amountOut = _swap(wewe, token, amount, 0, fee);
         return amountOut;
     }
 
     function sell(uint256 amount, address token, bytes calldata extraData) external returns (uint256) {
-        uint256 amountOut = _swap(token, wewe, amount, 0);
+        uint24 fee = uint24(uint256(keccak256(extraData)));
+        uint256 amountOut = _swap(token, wewe, amount, 0, fee);
         return amountOut;
     }
 
@@ -39,7 +29,8 @@ contract Uniswapv3 is IAMM, Ownable {
         address tokenIn,
         address tokenOut,
         uint256 amountIn,
-        uint256 amountOutMinimum
+        uint256 amountOutMinimum,
+        uint24 fee
     ) private returns (uint256 amountOut) {
         address pool = IUniswapv3(router).getPool(tokenIn, tokenOut, fee);
         require(pool != address(0), "Uniswapv3: Pool not found");
