fa35829c4924a9529eee1acdcc379aa9470bcf2e
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-10 13:55:36 +1000

    feat: add guards

--- contracts/core/merge/MergeWithMarket.sol
+++ contracts/core/merge/MergeWithMarket.sol
@@ -8,7 +8,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract MergeWithMarket is GenericMerge {
     constructor(address _wewe, address token) GenericMerge(_wewe, token) {}
 
-    function mergeAndSell(uint256 amount, IAMM amm, bytes calldata extraData) external {
+    function mergeAndSell(uint256 amount, IAMM amm, bytes calldata extraData) external nonReentrant {
         uint256 balance = IERC20(_token).balanceOf(msg.sender);
         require(balance >= amount, "MergeWithMarket: Insufficient balance to eat");
 
