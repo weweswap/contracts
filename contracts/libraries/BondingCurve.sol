f98076dd3400e59443bda323bbbb100301f58f95
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-10-31 07:06:10 +1000

    Refactor MergeWithMarket contract: Remove unnecessary functions and setTreasury variable

--- contracts/libraries/BondingCurve.sol
+++ contracts/libraries/BondingCurve.sol
@@ -5,9 +5,9 @@ library BondingCurve {
     // Function to simulate adding 1 more token and get the new price
     function calculateTokensOut(uint256 x, uint256 X, uint256 Y) public pure returns (uint256) {
         // Update the virtual balance for Token
-        // (x+X) where x is the additional FOMO
+        // (x+X) where x is the additional token and X is the current token balance
         uint256 newTokenBalance = X + x;
-        require(newTokenBalance > 0, "_calculateTokensOut: newTokenBalance must be greater than zero");
+        require(newTokenBalance > 0, "calculateTokensOut: newTokenBalance must be greater than zero");
 
         // y = (x*Y) / (x+X)
         uint256 y = (x * Y) / newTokenBalance;
