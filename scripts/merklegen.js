eac09d2d9d19d49dd3c096d6fd92f5e6eb9d2a00
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2024-11-01 20:19:06 +1000

    Enhance whitelist generation: Validate addresses and convert amounts to wei

--- scripts/merklegen.js
+++ scripts/merklegen.js
@@ -1,20 +1,46 @@
 const { StandardMerkleTree } = require("@openzeppelin/merkle-tree");
 const fs = require("fs");
+const ethers = require("ethers");
 
-const genProof = (csvPath, outputPath) => {
+const genProof = (csvPath, decimals, outputPath) => {
 	const csv = fs.readFileSync(csvPath, "utf-8");
 	const lines = csv.split("\n").filter(Boolean);
-	const whiteList = lines.map((line) => line.split(","));
+	const whiteList = [];
+
+	for (let i = 0; i < lines.length; i++) {
+		
+		// use regex to split by comma, but ignore commas within quotes
+		const row = lines[i].match(/(".*?"|[^",]+)(?=\s*,|\s*$)/g);
+
+		let address = row[0];
+		// remove quotes
+		address = address.replace(/"/g, '');
+
+		// test if address is valid using ethers
+		if (!ethers.isAddress(address)) {
+			console.log("Invalid address:", address);
+			continue;
+		}
+
+		let amount = row[1];
+
+		// remove commas
+		amount = amount.replace(/,/g, "");
+
+		// remove quotes
+		amount = amount.replace(/"/g, "");
+
+		// convert amount to wei
+		const amountAsBigInt = ethers.parseUnits(amount, decimals);
+
+		whiteList[i] = [address, amountAsBigInt.toString()];
+	}
+
+
 	const tree = StandardMerkleTree.of(whiteList, ["address", "uint256"]);
 	const rootHash = tree.root;
 
 	console.log("Root hash:", rootHash);
-
-	// const output = {
-	// 	rootHash,
-	// 	proofs: tree.elements.map((_, i) => tree.getProof(i))
-	// };
-
 	const output = [];
 
 	for (const [i, v] of tree.entries()) {
@@ -32,7 +58,7 @@ const genProof = (csvPath, outputPath) => {
 	
 	console.table(output);
 	console.log("Proofs generated and saved to", outputPath);
-
 }
 
-genProof("./whitelist.csv", "whitelist.json");
+// genProof("./whitelist.csv", "whitelist.json");
+genProof("./scripts/csvs/export-tokenholders-for-contract-0xd327d36eb6e1f250d191cd62497d08b4aaa843ce.csv", 9, "whitelist.json");
